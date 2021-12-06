cf4746d50a4500f7f85dcaaf337e99dad890435e
chemicalwill <chemicalwill92@gmail.com>
2021-12-05 20:27:48 -0600

    Solved Day 3 2021 part 2

--- day_03_2021.py
+++ day_03_2021.py
@@ -4,28 +4,59 @@ import os
 from pathlib import Path
 
 
-def power_consumption(lsts):
+def get_gamma_rate(data):
     gamma_rate = ''
-    epsilon_rate = ''
-    for x in range(len(lsts[0])):
-        bits = [lst[x] for lst in lsts]
-        i = bits.count('1')
-        o = bits.count('0')
-        if i > o:
+    for i in range(len(data[0])):
+        bits = [s[i] for s in data]
+        if bits.count('1') > bits.count('0'):
             gamma_rate += '1'
-            epsilon_rate += '0'
         else:
             gamma_rate += '0'
-            epsilon_rate += '1'
-    return int(gamma_rate, 2) * int(epsilon_rate, 2)
+    return gamma_rate
+
+
+def complement(s):
+    return ''.join(['1' if x == '0' else '0' for x in list(s)])
+
+
+def get_life_support_rating(data):
+    n = len(data[0])
+
+    o2_rating = data.copy()
+    for i in range(n):
+        bits = [s[i] for s in o2_rating]
+        if bits.count('1') >= bits.count('0'):
+            o2_rating = [s for s in o2_rating if s[i] == '1']
+        else:
+            o2_rating = [s for s in o2_rating if s[i] == '0']
+        if len(o2_rating) == 1:
+            break
+
+    co2_rating = data.copy()
+    for i in range(n):
+        bits = [s[i] for s in co2_rating]
+        if bits.count('1') >= bits.count('0'):
+            co2_rating = [s for s in co2_rating if s[i] == '0']
+        else:
+            co2_rating = [s for s in co2_rating if s[i] == '1']
+        if len(co2_rating) == 1:
+            break
+
+    return int(o2_rating.pop(), 2) * int(co2_rating.pop(), 2)
 
 
 def main():
     os.chdir(Path(__file__).parent)
-    # with open('test_input.txt', 'r') as f:
     with open('day_03_2021_input.txt', 'r') as f:
-        diagnostic_report = [list(line.strip()) for line in f.readlines()]
-    print(power_consumption(diagnostic_report))
+        diagnostic_report = [line.strip() for line in f.readlines()]
+
+    gamma_rate = get_gamma_rate(diagnostic_report)
+    epsilon_rate = complement(gamma_rate)
+    power_consumption = int(gamma_rate, 2) * int(epsilon_rate, 2)
+    print(power_consumption)
+
+    print(get_life_support_rating(diagnostic_report))
+    return
 
 
 if __name__ == "__main__":
