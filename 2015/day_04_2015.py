1e8b7df6d3ab3d65b9d172458b7fa3d59cfb1999
Will Hedges <swagbotbilly@gmail.com>
2022-08-14 22:20:42 -0500

    solved day 04 2015

--- 2015/day_04_2015.py
+++ 2015/day_04_2015.py
@@ -3,16 +3,21 @@
 import hashlib
 
 
-def find_lowest_number_hash(secret_key):
-    for i in range(1_000_000):
-        hash = hashlib.md5((secret_key + str(i).zfill(5)).encode("utf-8")).hexdigest()
-        if hash.startswith("00000"):
+def find_lowest_number_hash(secret_key, n_leading_zeros):
+    i = 0
+    while True:
+        hash = hashlib.md5((secret_key + str(i).zfill(n_leading_zeros)).encode("utf-8")).hexdigest()
+        if hash.startswith("0" * n_leading_zeros):
             return i
+        i += 1
 
 
 def main():
+    puzzle_key = "ckczppom"
     # part one
-    print(find_lowest_number_hash("ckczppom"))
+    print(find_lowest_number_hash(puzzle_key, 5))
+    # part two
+    print(find_lowest_number_hash(puzzle_key, 6))
 
 
 if __name__ == "__main__":
